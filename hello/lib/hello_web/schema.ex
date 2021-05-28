ef8afefd2446e766c7968d5f702124ddcacb8a77
jjwastaken <45520490+jjwastaken@users.noreply.github.com>
2021-05-27 22:39:45 -0700

    Can create users in graphiql

--- hello/lib/hello_web/schema.ex
+++ hello/lib/hello_web/schema.ex
@@ -15,7 +15,11 @@ defmodule HelloWeb.Schema do
   end
 
   # change/write data
-  # mutation do
-
-  # end
+  mutation do
+    @desc "Register a new user"
+    field :register_user, type: :user_type do
+      arg(:input, non_null(:user_input_type))
+      resolve(&Resolvers.UserResolver.register_user/3)
+    end
+  end
 end
